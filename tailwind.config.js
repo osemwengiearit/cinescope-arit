2b0556693b2b8a57e5c308befab691c65c25e412
Osemwengie Arit Bright <uyearit23@gmail.com>
2026-03-30 15:42:29 +0100

    Enhance tailwind.config.js with TypeScript and theme settings
    Updated Tailwind CSS configuration to include TypeScript support and added custom theme settings.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        screens: {
+            sm: "340px",
+            md: "540px",
+            lg: "768px",
+            xl: "1180px",
+        },
+        extend: {
+            colors: {
+                cream: "#EAB308",
+                grass: "#198754",
+            },
+        },
+        fontFamily: {
+            Jost: ["Jost", "sans-serif"],
+            Comic: ["Comic Neue", "cursive"],
+        },
+        containr: { center: true },
+    },
+    plugins: [],
 };
